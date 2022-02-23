c066011fb2fc569283126d23f10dba9ef9a10d13
Utkarsh Sharma <rickysharma1111@gmail.com>
2022-02-23 21:54:28 +0530

    Some minor changes

---
+++ lib/presentation/cubit/tempfeature_state.dart
@@ -0,0 +1,10 @@
+part of 'tempfeature_cubit.dart';
+
+abstract class TempfeatureState extends Equatable {
+  const TempfeatureState();
+
+  @override
+  List<Object> get props => [];
+}
+
+class TempfeatureInitial extends TempfeatureState {}
